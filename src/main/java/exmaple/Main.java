bfc988636691564ef6976092e9b23cb091fdaf7b
saba75 <saba.safavi75@gmail.com>
2021-05-09 11:33:16 +0430

    fix issue

--- src/main/java/exmaple/Main.java
+++ src/main/java/exmaple/Main.java
@@ -1,11 +1,13 @@
 package exmaple;
 
+import java.io.IOException;
+
 /**
  * Created By Khojasteh on 7/30/2019
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ChatMain chatMain = new ChatMain();
         chatMain.init();
 
